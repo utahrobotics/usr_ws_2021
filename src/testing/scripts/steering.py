8a3723ade46df28186339827ff4ea7da08385266
fredtremonti <fredt1797@gmail.com>
2022-02-02 19:18:55 -0700

    added radial steering

--- src/testing/scripts/steering.py
+++ src/testing/scripts/steering.py
@@ -3,7 +3,9 @@
 # interfaces with user input to control the robot.
 
 from tokenize import Double
-
+import vector
+import math
+import numpy as np
 
 class LocCtlr:
 # take range from -1 to 1 and traslate that to the angle we need to move the wheels. 
@@ -19,16 +21,31 @@ class LocCtlr:
         self.rightInput=0.0
 
     def tankSteer(self,_leftJoystick:Double, _rightJoystick:Double): 
-        self.leftMotion= _leftJoystick*self.scale
-        self.rightMotion= _rightJoystick*self.scale
-        return (self.leftMotion,self.rightMotion)
+        self.leftMotion = _leftJoystick * self.scale
+        self.rightMotion = _rightJoystick * self.scale
+        angles = [0, 0, 0, 0]
+        velocities = [self.leftMotion, self.leftMotion, self.rightMotion, self.rightMotion]
+        return (angles, velocities)
 
-    def translationControl(self,_leftJoystick:Double, _rightTrigger:Double):
+    def translationControl(self, _leftJoystick:Double, _rightTrigger:Double):
         pass
 
-    def radialSteer(self,_leftJoystick:Double, _rightTrigger:Double):
-        pass
+    def radialSteer(self, _leftJoystick:Double, _rightTrigger:Double):
+        self.wheelCenter = [0, 0.5]
+        self.steerIntensity = [(_leftJoystick * 2)**3, 0]
+        angleVector = np.subtract(self.wheelCenter, self.steerIntensity)
+        angleVectorUnit = angleVector / np.linalg.norm(angleVector)
+        axisX = [1, 0]
+        dot = np.dot(angleVectorUnit, axisX)
+        angle = np.arccos(dot) * 180 / math.pi
+        angles = [angle, -angle, angle, -angle]
+        velocities = [_rightTrigger, _rightTrigger, _rightTrigger, _rightTrigger]
+        return (angles, velocities)
 
 if __name__ == "__main__":
     controller = LocCtlr(10)
     print (controller.tankSteer(1,5))
+    print(controller.radialSteer(0, 0))
+    print(controller.radialSteer(-1, 0))
+    print(controller.radialSteer(1, 0))
+    print(controller.radialSteer(0.5, 0))
