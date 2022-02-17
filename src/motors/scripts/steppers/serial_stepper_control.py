b81b0cbdce072cb0a0fc25bfef8bec9ad4093a34
alexCharters <alexcharters45@gmail.com>
2022-02-16 18:50:29 -0700

    Minor fixes to serial controller

--- src/motors/scripts/steppers/serial_stepper_control.py
+++ src/motors/scripts/steppers/serial_stepper_control.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """
 This node is the communication layer betweeen the USR Ros subsystem and the stepper motor controllers.
 """
@@ -6,12 +7,12 @@ This node is the communication layer betweeen the USR Ros subsystem and the step
 #import rclpy
 #from rclpy.node import Node
 import rospy
-from rospy import Node
 import yaml
 import serial, time
 from enum import Enum
+import os
 
-from motion_controller_msgs.msg import Mobility
+from locomotion.msg import SteerAndThrottle
 
 
 class Command(Enum):
@@ -30,16 +31,16 @@ class SteeringSubscriber():
     def __init__(self):
         rospy.init_node('stepper_control_node')
         rospy.Subscriber(
-            'steering',
-            Mobility,
+            'locomotion',
+            SteerAndThrottle,
             self.listener_callback)
 
         # create controller instances for each for each of the motor bases from the config file
-        tmp_file = open('./config/stepper_config.yaml')
-        stepper_config = yaml.load(tmp_file, Loader=yaml.FullLoader)
+        tmp_file = open('./src/motors/scripts/steppers/config/stepper_config.yaml')
+        stepper_config = yaml.safe_load(tmp_file)
 
-        self.stepper_controller = StepperController(serial=stepper_config['serial'],
-                                                    steps=stepper_config['steps'])
+        self.stepper_controller = StepperController(stepper_config['serial'],
+                                                    stepper_config['steps'])
 
         # intilialize motors and blink for conformation
         self.stepper_controller.initMotors()
@@ -54,10 +55,11 @@ class SteeringSubscriber():
         # TODO: incorperate the state machince variables to decide if motors should be running or not
 
         # if motors are ready, set the new speed to each controller
-        self.stepper_controller.alignMotors(msg.front_left,
-                                            msg.front_right,
-                                            msg.back_left,
-                                            msg.back_right)
+        self.stepper_controller.alignMotors(msg.angles[0],
+                                            msg.angles[1],
+                                            msg.angles[2],
+                                            msg.angles[3]
+	)
 
 
 class StepperController():
@@ -65,9 +67,9 @@ class StepperController():
         This class holds the imformation relevant for controller a stepper motor contorller onboard the teensy device
     """
 
-    def __init__(self, serial, steps) -> None:
-        self.serial = serial  # the serial number for responding to the device
-        self._mc = serial.Serial(serial, 115200, timeout=.1)  # teh micro controller serial instance
+    def __init__(self, serial_number, steps):
+        self.serial = serial_number  # the serial number for responding to the device
+        self._mc = serial.Serial(serial_number, 115200, timeout=.1)  # teh micro controller serial instance
         time.sleep(1)  # give the connection a second to settle
 
     def alignMotors(self, fl, fr, bl, br):
