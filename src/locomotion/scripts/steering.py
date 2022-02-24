95d317cf919dd563a324efa1e475de98af762f92
fredtremonti <98926856+fredtremonti@users.noreply.github.com>
2022-02-23 18:50:05 -0700

    Have basic idea. Need further implementation on publishing message

--- src/locomotion/scripts/steering.py
+++ src/locomotion/scripts/steering.py
@@ -8,6 +8,7 @@ import math
 import numpy as np
 import rospy
 from std_msgs.msg import Header
+from std_msgs.msg import twist
 from sensor_msgs.msg import Joy
 from locomotion.msg import SteerAndThrottle
 
@@ -20,8 +21,10 @@ class LocCtlr:
     rightMotion =0.0
     leftMotion= 0.0
     wheetDist = 1.0
+    # how to set true false? Command line?
+    autonomous = False
     def __init__(self,Scale, _pub):
-	self.pub = _pub
+	    self.pub = _pub
         self.scale=Scale
         self.angle=0.0
         self.rightInput=0.0
@@ -32,12 +35,12 @@ class LocCtlr:
         angles = [0, 0, 0, 0]
         velocities = [self.leftMotion, self.leftMotion, self.rightMotion, self.rightMotion]
         H = Header()
-	H.stamp = rospy.Time.now()
-	msg = SteerAndThrottle()
-	msg.header = H
-	msg.angles = angles
-	msg.throttles = velocities
-	self.pub.publish(msg)
+        H.stamp = rospy.Time.now()
+        msg = SteerAndThrottle()
+        msg.header = H
+        msg.angles = angles
+        msg.throttles = velocities
+        self.pub.publish(msg)
         return (angles, velocities)
 
     def translationControl(self, _leftJoystick, _rightTrigger):
@@ -50,13 +53,13 @@ class LocCtlr:
         angle = np.arccos(dot) * 180 / math.pi
         angles = [angle, angle, angle, angle]
         velocities = [_rightTrigger, _rightTrigger, _rightTrigger, _rightTrigger]
-	H = Header()
-	H.stamp = rospy.Time.now()
-	msg = SteerAndThrottle()
-	msg.header = H
-	msg.angles = angles
-	msg.throttles = velocities
-	self.pub.publish(msg)
+        H = Header()
+        H.stamp = rospy.Time.now()
+        msg = SteerAndThrottle()
+        msg.header = H
+        msg.angles = angles
+        msg.throttles = velocities
+        self.pub.publish(msg)
         return (angles, velocities)
 
     def radialSteer(self, _leftJoystick, _rightTrigger):
@@ -69,13 +72,13 @@ class LocCtlr:
         angle = np.arccos(dot) * 180 / math.pi
         angles = [angle, -angle, angle, -angle]
         velocities = [_rightTrigger, _rightTrigger, _rightTrigger, _rightTrigger]
-	H = Header()
-	H.stamp = rospy.Time.now()
-	msg = SteerAndThrottle()
-	msg.header = H
-	msg.angles = angles
-	msg.throttles = velocities
-	self.pub.publish(msg)
+        H = Header()
+        H.stamp = rospy.Time.now()
+        msg = SteerAndThrottle()
+        msg.header = H
+        msg.angles = angles
+        msg.throttles = velocities
+        self.pub.publish(msg)
         return (angles, velocities)
 
 def callback(joy):
@@ -84,8 +87,19 @@ def callback(joy):
 if __name__ == "__main__":
 	pub = rospy.Publisher('locomotion', SteerAndThrottle, queue_size=10)
 	rospy.init_node('locomotion')
-	rospy.Subscriber("telemetry_joy", Joy, callback)
-   	controller = LocCtlr(1, pub)
+    rospy.Subscriber("telemetry_joy", Joy, callback)
+    #maybe need outside change global var to true or false
+	if autonomous == True:
+        #need method that publishes message to amee
+        pub = rospy.Publisher('autonomous_steer', twist, queue_size=10)
+        controller = LocCtlr(1, pub)
+        while autonomous == True:
+            # a method that publishes message to twist
+    else:
+   	    controller = LocCtlr(1, pub)
+        while autonomous == False:
+            # a method that publishes message to joystick
+
 	rospy.spin()
 	#i = 0
 	#while True:
