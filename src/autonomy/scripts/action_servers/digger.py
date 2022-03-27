eba44eec20bb7c0db753f4852ff09c1fd5e709cf
manglinux <shabouza030@gmail.com>
2022-03-27 11:39:06 -0600

    Renamed RetractArmAction to MoveArmAction

--- src/autonomy/scripts/action_servers/digger.py
+++ src/autonomy/scripts/action_servers/digger.py
@@ -1,11 +1,11 @@
 import rospy
 from abstract_server import AbstractActionServer
-from autonomy.msg import RetractArmAction
+from autonomy.msg import MoveArmAction
 
 
-class RetractArm(AbstractActionServer):
+class MoveArm(AbstractActionServer):
     def __init__(self):
-        AbstractActionServer.__init__(self, 'retract_arm', RetractArmAction)
+        AbstractActionServer.__init__(self, 'move_arm', MoveArmAction)
 
     def execute(self, goal):
         rospy.loginfo("Executed")
