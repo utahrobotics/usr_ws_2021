6977a342529fe3006f6a9af26250e7ed113399ed
BuildTools <unconfigured@null.spigotmc.org>
2022-05-11 00:20:10 -0600

    added digging set speed action server

--- src/locomotion/scripts/digging.py
+++ src/locomotion/scripts/digging.py
@@ -10,6 +10,8 @@ import numpy as np
 import rospy
 from std_msgs.msg import Float32
 from sensor_msgs.msg import Joy
+import actionlib
+from locomotion.msg import SetSpeedAction, SetSpeedFeedback, SetSpeedResult
 
 
 class DigCtlr:
@@ -17,7 +19,27 @@ class DigCtlr:
     def __init__(self):
         self.armPub = rospy.Publisher('arm_vel', Float32, queue_size=1)
         self.drumPub = rospy.Publisher('drum_vel', Float32, queue_size=1)
+
+        self.setDrumSpeedServer = actionlib.SimpleActionServer(
+            "set_drum_speed_as", SetSpeedAction, execute_cb=self.setDrumSpeed_cb, auto_start=False)
+        self.setDrumSpeedServer.start()
+        self.setArmSpeedServer = actionlib.SimpleActionServer(
+            "set_arm_speed_as", SetSpeedAction, execute_cb=self.setArmSpeed_cb, auto_start=False)
+        self.setArmSpeedServer.start()
+
         rospy.Subscriber("telemetry_joy", Joy, self.joyCallback, queue_size=1)
+
+    def setDrumSpeed_cb(self, goal):
+        self.drumPub.publish(Float32(goal.speed))
+        result = SetSpeedResult()
+        result.speed = goal.speed
+        self.a_server.set_succeeded(goal.speed)
+
+    def setArmSpeed_cb(self, goal):
+        self.armPub.publish(Float32(goal.speed))
+        result = SetSpeedResult()
+        result.speed = goal.speed
+        self.a_server.set_succeeded(goal.speed)
 
     def Dig(self,_leftTrigger, _rightTrigger, _leftBumper, _rightBumper):
         armVel = 0
