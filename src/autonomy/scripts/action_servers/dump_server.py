aabf5caf8ff0900d27cd7a96f97c23e4b8d6f4ac
manglinux <shabouza030@gmail.com>
2022-05-12 16:56:11 -0600

    simplified dump server

--- src/autonomy/scripts/action_servers/dump_server.py
+++ src/autonomy/scripts/action_servers/dump_server.py
@@ -1,34 +1,29 @@
 import rospy
 from abstract_server import AbstractActionServer
-from actionlib import SimpleActionClient
 from locomotion.msg import SetSpeedGoal, SetSpeedAction, SteerAndThrottle
 from autonomy.msg import DumpAction
+from std_msgs.msg import Float32
 from math import pi
 from time import sleep
 
 
 class DumpServer(AbstractActionServer):
     def __init__(self):
-        self._move_arm = SimpleActionClient('set_arm_speed_as', SetSpeedAction)
-        self._move_drum = SimpleActionClient('set_drum_speed_as', SetSpeedAction)
-        timeout = rospy.Duration(3)
-        self._move_arm.wait_for_server(timeout)
-        self._move_drum.wait_for_server(timeout)
-
+        self._arm_pub = rospy.Publisher('arm_vel', Float32, queue_size=1)
+        self._drum_pub = rospy.Publisher('drum_vel', Float32, queue_size=1)
         self._drive_pub = rospy.Publisher('locomotion', SteerAndThrottle, queue_size=5)
 
         AbstractActionServer.__init__(self, "Dump", DumpAction)
     
     def execute(self, goal):
         # lift arm
-        goal = SetSpeedGoal()
-        goal.speed = -1.0
+        msg = Float32(-1)
         for _ in range(6):
-            self._move_arm.send_goal(goal)
+            self._arm_pub.publish(msg)
             sleep(0.5)
-        goal = SetSpeedGoal()       # Could just edit speed without reinstancing but be safe
-        goal.speed = 0.0
-        self._move_arm.send_goal(goal)
+        self._arm_pub.publish(Float32(0))
+        
+        sleep(0.5)
         
         # drive forward
         goal = SteerAndThrottle()
@@ -40,16 +35,15 @@ class DumpServer(AbstractActionServer):
         goal.angles = [90, 90, 90, 90]
         goal.throttles = [0, 0, 0, 0]
         self._drive_pub.publish(goal)
+        
+        sleep(0.5)
 
         # unload
-        goal = SetSpeedGoal()
-        goal.speed = -1.0
-        self._move_drum.send_goal(goal)
+        self._drum_pub.publish(Float32(-1))
         sleep(5)
-        goal = SetSpeedGoal()
-        goal.speed = 0
+        self._drum_pub.publish(Float32(0))
         
-        self._move_drum.send_goal(goal)
+        sleep(0.5)
         
         # drive back
         goal = SteerAndThrottle()
@@ -61,13 +55,12 @@ class DumpServer(AbstractActionServer):
         goal.angles = [90, 90, 90, 90]
         goal.throttles = [0, 0, 0, 0]
         self._drive_pub.publish(goal)
+        
+        sleep(0.5)
 
         # lower arm
-        goal = SetSpeedGoal()
-        goal.speed = 1.0
+        msg = Float32(1)
         for _ in range(6):
-            self._move_arm.send_goal(goal)
+            self._arm_pub.publish(msg)
             sleep(0.5)
-        goal = SetSpeedGoal()
-        goal.speed = 0.0
-        self._move_arm.send_goal(goal)
+        self._arm_pub.publish(Float32(0))
