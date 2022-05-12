4c4c7eb08560d51fd11885db5994a26bd57f6dc6
alexCharters <alexcharters45@gmail.com>
2022-05-11 19:19:45 -0700

    corrected indentation

--- src/locomotion/scripts/digging.py
+++ src/locomotion/scripts/digging.py
@@ -10,57 +10,22 @@ import numpy as np
 import rospy
 from std_msgs.msg import Float32
 from sensor_msgs.msg import Joy
-import actionlib
-from locomotion.msg import SetSpeedAction, SetSpeedFeedback, SetSpeedResult
 
 
 class DigCtlr:
 # take range from -1 to 1 and traslate that to the angle we need to move the wheels.
     def __init__(self):
         self.armPub = rospy.Publisher('arm_vel', Float32, queue_size=1)
-<<<<<<< HEAD
-    self.drumPub = rospy.Publisher('drum_vel', Float32, queue_size=1)
-    rospy.Subscriber("telemetry_joy", Joy, self.joyCallback, queue_size=1)
-=======
         self.drumPub = rospy.Publisher('drum_vel', Float32, queue_size=1)
-
-        self.setDrumSpeedServer = actionlib.SimpleActionServer(
-            "set_drum_speed_as", SetSpeedAction, execute_cb=self.setDrumSpeed_cb, auto_start=False)
-        self.setDrumSpeedServer.start()
-        self.setArmSpeedServer = actionlib.SimpleActionServer(
-            "set_arm_speed_as", SetSpeedAction, execute_cb=self.setArmSpeed_cb, auto_start=False)
-        self.setArmSpeedServer.start()
-
         rospy.Subscriber("telemetry_joy", Joy, self.joyCallback, queue_size=1)
-
-    def setDrumSpeed_cb(self, goal):
-        self.drumPub.publish(Float32(goal.speed))
-        result = SetSpeedResult()
-        result.speed = goal.speed
-        self.a_server.set_succeeded(goal.speed)
-
-    def setArmSpeed_cb(self, goal):
-        self.armPub.publish(Float32(goal.speed))
-        result = SetSpeedResult()
-        result.speed = goal.speed
-        self.a_server.set_succeeded(goal.speed)
->>>>>>> origin/main
 
     def Dig(self,_leftTrigger, _rightTrigger, _leftBumper, _rightBumper):
         armVel = 0
         drumVel = (_leftTrigger - _rightTrigger)/2
-<<<<<<< HEAD
         if _leftBumper ^ _rightBumper:
             armVel = 1 if _rightBumper else -1
         self.armPub.publish(Float32(armVel))
         self.drumPub.publish(Float32(drumVel))
-=======
-        if(_leftBumper ^ _rightBumper):
-            armVel = 1 if _rightBumper else -1
-            self.armPub.publish(Float32(armVel))
-            self.drumPub.publish(Float32(drumVel))
-
->>>>>>> origin/main
         return (armVel, drumVel)
         
     def joyCallback(self, joy):
@@ -81,8 +46,8 @@ class DigCtlr:
 #            locController.tankSteer(left_speed, right_speed)
 
 if __name__ == "__main__":
-    rospy.init_node('locomotion')
     locController = DigCtlr()
+    rospy.init_node('locomotion')
     rospy.spin()
     #i = 0
     #while True:
