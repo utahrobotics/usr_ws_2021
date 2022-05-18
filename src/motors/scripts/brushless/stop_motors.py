ec19503ad4c7f0f1c8fac3305b8973f3872b86bd
Manglemix <shabouza030@gmail.com>
2022-05-18 15:45:06 -0600

    improved

--- src/motors/scripts/brushless/stop_motors.py
+++ src/motors/scripts/brushless/stop_motors.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python
 
 import Jetson.GPIO as GPIO
+import time
 
 enable_pin = 7
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(channels=enable_pin, direction=GPIO.OUT, initial=GPIO.LOW)
 GPIO.output(enable_pin, GPIO.LOW)
+time.sleep(1)
+GPIO.output(enable_pin, GPIO.HIGH)
+time.sleep(1)
+GPIO.output(enable_pin, GPIO.LOW)
+time.sleep(1)
+GPIO.output(enable_pin, GPIO.HIGH)
+time.sleep(1)
+GPIO.output(enable_pin, GPIO.LOW)
+time.sleep(1)
+GPIO.output(enable_pin, GPIO.HIGH)
+time.sleep(1)
+GPIO.output(enable_pin, GPIO.LOW)
+time.sleep(1)
+GPIO.output(enable_pin, GPIO.HIGH)
+time.sleep(1)
+GPIO.output(enable_pin, GPIO.LOW)
+time.sleep(1)
