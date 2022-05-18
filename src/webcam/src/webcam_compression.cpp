754140b46c5ecaf68d03844181d3d6f651a42efc
Manglemix <shabouza030@gmail.com>
2022-05-18 15:19:40 -0600

    fix4

--- src/webcam/src/webcam_compression.cpp
+++ src/webcam/src/webcam_compression.cpp
@@ -3,9 +3,7 @@
 #include <image_transport/image_transport.h>
 
 
-ros::NodeHandle nh;
-image_transport::ImageTransport it(nh);
-image_transport::Publisher pub = it.advertise("/webcam/compressed", 1);
+image_transport::Publisher pub;
 
 
 void imageCallback(const sensor_msgs::ImageConstPtr& msg)
@@ -13,10 +11,13 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg)
   pub.publish(msg);
 }
 
-image_transport::Subscriber sub = it.subscribe("/webcam/image_raw", 1, imageCallback);
 
 
 int main(int argc, char *argv[]) {
 	ros::init(argc, argv, "webcam_compression");
+	ros::NodeHandle nh;
+	image_transport::ImageTransport it(nh);
+	image_transport::Subscriber sub = it.subscribe("/webcam/image_raw", 1, imageCallback);
+	pub = it.advertise("/webcam/compressed", 1);
 	ros::spin();
 }
