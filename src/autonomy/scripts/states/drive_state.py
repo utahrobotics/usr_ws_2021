128d07bcc4ee387ba0173557edd531f287e1ceef
Manglemix <shabouza030@gmail.com>
2022-05-23 06:22:32 -0600

    move base cancels on manual

--- src/autonomy/scripts/states/drive_state.py
+++ src/autonomy/scripts/states/drive_state.py
@@ -6,8 +6,8 @@ from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
 class DriveState(ExtendedState):
 	def __init__(self):
-		self.move_clients = actionlib.SimpleActionClient('move_base', MoveBaseAction)
-		self.move_clients.wait_for_server(rospy.Duration(3))
+		self._move_client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+		self._move_client.wait_for_server(rospy.Duration(3))
 		ExtendedState.__init__(
 			self,
 			outcomes=['reached_dig_site', 'reached_bin', 'manual'],
@@ -21,15 +21,17 @@ class DriveState(ExtendedState):
 		goal.target_pose.pose.position.x = x
 		goal.target_pose.pose.position.y = y
 		# goal.target_pose.pose.orientation.w = 1.0
-		self.move_clients.send_goal(goal)
-		return self.wait_for_action_result(self.move_clients)
+		self._move_client.send_goal(goal)
+		return self.wait_for_action_result(self._move_client)
 	
 	def execute(self, userdata):
 		userdata.current_state = 'Drive'
 		if userdata.driving_to_site:
 			if self._move_to(1, 5.64):
+				self._move_client.cancel_goal()
 				return 'manual'
 		elif self._move_to(0, 1.1):
+			self._move_client.cancel_goal()
 			return 'manual'
 		rospy.logwarn('Drove to bin')
 		return 'reached_bin'
