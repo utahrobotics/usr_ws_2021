b2aee96a204931c0f11123f17a3709ca9f0acf6e
Manglemix <shabouza030@gmail.com>
2022-05-23 14:51:43 -0600

    Update state

--- src/autonomy/scripts/states/initialize_state.py
+++ src/autonomy/scripts/states/initialize_state.py
@@ -1,12 +1,12 @@
 import rospy
 from extended_state import ExtendedState
-from motors.msg import FakeInitAction, FakeInitGoal
+from autonomy.msgs import InitializeAction, InitializeGoal
 from actionlib import SimpleActionClient
 
 
 class InitializeState(ExtendedState):
 	def __init__(self):
-		self.fake_init_client = SimpleActionClient("fake_init_as", FakeInitAction)
+		self.init_client = SimpleActionClient("initialize", InitializeAction)
 		ExtendedState.__init__(
 			self,
 			outcomes=['finished', 'manual'],
@@ -15,11 +15,9 @@ class InitializeState(ExtendedState):
 		)
 	
 	def execute(self, userdata):
-		rospy.logwarn("Fake initing")
-		goal = FakeInitGoal()
-		goal.goal = True
-		self.fake_init_client.send_goal(goal)
-		if self.wait_for_action_result(self.fake_init_client):
+		rospy.logwarn("Initing")
+		self.init_client.send_goal(InitializeGoal())
+		if self.wait_for_action_result(self.init_client):
 			return 'manual'
-		rospy.logwarn('Fake initialized')
+		rospy.logwarn('Initialized')
 		return 'finished'
