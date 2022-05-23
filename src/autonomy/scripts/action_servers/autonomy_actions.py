d16de7d852e7e153a3d77a70bf8ac3bbe3194977
Manglemix <shabouza030@gmail.com>
2022-05-23 15:35:58 -0600

    updated autonomy_actions

--- src/autonomy/scripts/action_servers/autonomy_actions.py
+++ src/autonomy/scripts/action_servers/autonomy_actions.py
@@ -3,6 +3,7 @@
 import rospy
 
 from initialize_server import InitializeServer
+from arm_server import MoveArm, DigServer, DumpServer
 from dump_server import DumpServer
 
 
@@ -10,7 +11,9 @@ if __name__ == "__main__":
 	rospy.init_node("autonomy_actions")
 	init = InitializeServer()
 	dump_server = DumpServer()
-	# init.start()
+	dig_server = DigServer()
+	init.start()
 	dump_server.start()
+	dig_server.start()
 	rospy.logwarn("Action servers started")
 	rospy.spin()
