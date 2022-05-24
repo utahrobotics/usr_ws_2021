fcf5557efe3c81923b1d35a293cef40af21e5bc5
Your Name <you@example.com>
2022-05-24 08:35:25 -0600

    everything working!

--- src/motors/scripts/steppers/serial_com.py
+++ src/motors/scripts/steppers/serial_com.py
@@ -3,7 +3,7 @@ from enum import Enum
 import struct
 import threading
 
-COM = "COM10"
+COM = "/dev/stepper_teensy"
 arduino = serial.Serial(COM, 115200, timeout=.1)
 
 int_to_four_bytes = struct.Struct('<I').pack
