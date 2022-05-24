dad3d9a6876f302c743a6aec2dc5391605b06ed2
Manglemix <shabouza030@gmail.com>
2022-05-24 05:43:27 -0600

    update angle sense

--- src/motors/scripts/brushed/angle_sense.py
+++ src/motors/scripts/brushed/angle_sense.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python
 import smbus
 import time
-#import matplotlib.pyplot as plt
-#from matplotlib.animation import FuncAnimation
-import numpy as np
 import rospy
-import collections
+import math
 from std_msgs.msg import Float32
-
-
 
 
 # Register and other configuration values:
@@ -21,45 +16,48 @@ ADS1x15_CONFIG_OS_SINGLE       = 0x8000
 ADS1x15_CONFIG_MUX_OFFSET      = 12
 # Mapping of gain values to config register values.
 ADS1x15_CONFIG_GAIN = {
-    2/3: 0x0000,
-    1:   0x0200,
-    2:   0x0400,
-    4:   0x0600,
-    8:   0x0800,
-    16:  0x0A00
+	2/3: 0x0000,
+	1:   0x0200,
+	2:   0x0400,
+	4:   0x0600,
+	8:   0x0800,
+	16:  0x0A00
 }
 ADS1x15_CONFIG_MODE_CONTINUOUS  = 0x0000
 ADS1x15_CONFIG_MODE_SINGLE      = 0x0100
 # Mapping of data/sample rate to config register values for ADS1015 (faster).
 ADS1015_CONFIG_DR = {
-    128:   0x0000,
-    250:   0x0020,
-    490:   0x0040,
-    920:   0x0060,
-    1600:  0x0080,
-    2400:  0x00A0,
-    3300:  0x00C0
+	128:   0x0000,
+	250:   0x0020,
+	490:   0x0040,
+	920:   0x0060,
+	1600:  0x0080,
+	2400:  0x00A0,
+	3300:  0x00C0
 }
 # Mapping of data/sample rate to config register values for ADS1115 (slower).
 ADS1115_CONFIG_DR = {
-    8:    0x0000,
-    16:   0x0020,
-    32:   0x0040,
-    64:   0x0060,
-    128:  0x0080,
-    250:  0x00A0,
-    475:  0x00C0,
-    860:  0x00E0
+	8:    0x0000,
+	16:   0x0020,
+	32:   0x0040,
+	64:   0x0060,
+	128:  0x0080,
+	250:  0x00A0,
+	475:  0x00C0,
+	860:  0x00E0
 }
 ADS1x15_CONFIG_COMP_WINDOW      = 0x0010
 ADS1x15_CONFIG_COMP_ACTIVE_HIGH = 0x0008
 ADS1x15_CONFIG_COMP_LATCHING    = 0x0004
 ADS1x15_CONFIG_COMP_QUE = {
-    1: 0x0000,
-    2: 0x0001,
-    4: 0x0002
+	1: 0x0000,
+	2: 0x0001,
+	4: 0x0002
 }
 ADS1x15_CONFIG_COMP_QUE_DISABLE = 0x0003
+
+arm_length = 0.569
+drum_radius = 0.195
 
 
 class AngleSensor:
@@ -76,14 +74,14 @@ class AngleSensor:
 		config |= (mux & 0x07) << ADS1x15_CONFIG_MUX_OFFSET
 		# Validate the passed in gain and then set it in the config.
 		if self.gain not in ADS1x15_CONFIG_GAIN:
-		    raise ValueError('Gain must be one of: 2/3, 1, 2, 4, 8, 16')
+			raise ValueError('Gain must be one of: 2/3, 1, 2, 4, 8, 16')
 		config |= ADS1x15_CONFIG_GAIN[self.gain]
 		# Set the mode (continuous or single shot).
 		config |= mode
 		# Get the default data rate if none is specified (default differs between
 		# ADS1015 and ADS1115).
 		if data_rate is None:
-		    data_rate = 128
+			data_rate = 128
 		# Set the data rate (this is controlled by the subclass as it differs
 		# between ADS1015 and ADS1115).
 		config |= self._data_rate_config(data_rate)
@@ -97,7 +95,6 @@ class AngleSensor:
 		# Retrieve the result.
 		result = self.bus.read_i2c_block_data(ADS1x15_DEFAULT_ADDRESS, ADS1x15_POINTER_CONVERSION, 2)
 		return self._conversion_value(result[1], result[0])
-
 
 	def start_adc(self, channel, data_rate=None):
 		"""Start continuous ADC conversions on the specified channel (0-3). Will
@@ -121,7 +118,7 @@ class AngleSensor:
 
 	def _data_rate_config(self, data_rate):
 		if data_rate not in ADS1115_CONFIG_DR:
-		    raise ValueError('Data rate must be one of: 8, 16, 32, 64, 128, 250, 475, 860')
+			raise ValueError('Data rate must be one of: 8, 16, 32, 64, 128, 250, 475, 860')
 		return ADS1115_CONFIG_DR[data_rate]
 
 	def _conversion_value(self, low, high):
@@ -129,7 +126,7 @@ class AngleSensor:
 		value = ((high & 0xFF) << 8) | (low & 0xFF)
 		# Check for sign bit and turn into a negative value if set.
 		if value & 0x8000 != 0:
-		    value -= 1 << 16
+			value -= 1 << 16
 		return value
 
 	def computeVolts(self, counts):
@@ -153,10 +150,9 @@ class AngleSensor:
 		upper_bound = vdd*.9
 		lower_bound = vdd*.1
 		slope = (270-90)/(upper_bound-lower_bound)
-		angle = (output-lower_bound)*slope
+		angle = (output-lower_bound)*slope+57.8
 		return angle
 
-	
 
 if __name__ == "__main__":
 	sensor = AngleSensor()
@@ -177,9 +173,19 @@ if __name__ == "__main__":
 	# ani = FuncAnimation(fig, animate, interval=10)
 	# plt.show()
 
-	pub = rospy.Publisher('/sensors/angleSensor/angle', Float32 , queue_size=10)
+	pub = rospy.Publisher('/sensors/angleSensor/angle', Float32, queue_size=10)
+	arm_depth_pub = rospy.Publisher('/sensors/angleSensor/depth', Float32, queue_size=10)
 	rospy.init_node('angleSensor')
 	r = rospy.Rate(10) # 10hz
+	NEUTRAL_ARM_HEIGHT = 0.30480
+	print("start")
+	
 	while not rospy.is_shutdown():
-		pub.publish(sensor.computeDegrees(5, sensor.computeVolts(sensor.get_last_result())))
+		angle = sensor.computeDegrees(5, sensor.computeVolts(sensor.get_last_result()))
+		theta = angle - 180
+		depth = arm_length*math.tan(theta) - drum_radius + NEUTRAL_ARM_HEIGHT
+		print(angle)
+		print(depth)
+		pub.publish(angle)
+		arm_depth_pub.publish(depth)
 		r.sleep()
